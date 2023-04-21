36468190e844466f7168b867ed8bfeabc8a4053f
Dorottya <dorottyabalogh89@gmail.com>
2023-04-21 18:23:34 +0200

    change header, add back to top button and animations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navIcon = document.querySelector('.nav-icon');
 const navLinks = document.querySelectorAll('.nav-link');
 const cardImages = document.querySelectorAll('.card-image');
+const toTopBtn = document.querySelector('.back-to-top');
 
 function toggleNav() {
   document.querySelector('body').classList.toggle('no-scroll');
@@ -30,3 +31,46 @@ if (window.innerWidth >= 1024) {
 const today = new Date();
 const year = today.getFullYear();
 document.getElementById('year').textContent = year;
+
+function fadeUpObserverCallback(elsToWatch) {
+  elsToWatch.forEach(el => {
+    if (el.isIntersecting) {
+      el.target.classList.add('scale-up');
+      fadeUpObserver.unobserve(el.target);
+      el.target.addEventListener(
+        'transitionend',
+        () => {
+          el.target.classList.remove('scale-up');
+        },
+        { once: true }
+      );
+    }
+  });
+}
+
+const fadeUpObserverOptions = {
+  threshold: 0.6,
+};
+
+const fadeUpObserver = new IntersectionObserver(fadeUpObserverCallback, fadeUpObserverOptions);
+
+if (window.innerWidth < 1024) {
+  cardImages.forEach(item => {
+    fadeUpObserver.observe(item);
+  });
+}
+
+function showOnScroll() {
+  const y = window.scrollY;
+  if (y >= 1000) {
+    toTopBtn.className = 'back-to-top show';
+  } else {
+    toTopBtn.className = 'back-to-top hide';
+  }
+}
+
+window.addEventListener('scroll', showOnScroll);
+
+toTopBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
